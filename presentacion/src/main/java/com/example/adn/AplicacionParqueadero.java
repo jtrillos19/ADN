abfa8680d033df81093f8d6ce954c6cb305e2bb6
Jesus Trillos <jesus.trillos@ceiba.com.co>
2021-05-18 21:54:39 -0500

    Se crea la clase aplicación parqueadero por la cual va iniciar la app. Se crea el modulo vehículo para la inyección de dependencias.

---
+++ presentacion/src/main/java/com/example/adn/AplicacionParqueadero.java
@@ -0,0 +1,9 @@
+package com.example.adn;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class AplicacionParqueadero extends Application {
+}
---
+++ presentacion/src/main/java/com/example/adn/moduloid/ModuloVehiculo.java
@@ -0,0 +1,23 @@
+package com.example.adn.moduloid;
+
+import com.example.domain.repositorio.CarroRepositorio;
+import com.example.domain.repositorio.MotocicletaRepositorio;
+import com.example.infraestructura.repositorio.CarroRepositorioRoom;
+import com.example.infraestructura.repositorio.MotocicletaRepositorioRoom;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
+
+@Module
+@InstallIn({ActivityComponent.class})
+public abstract class ModuloVehiculo {
+
+    @Binds
+    public abstract CarroRepositorio bindCarroRepositorio(CarroRepositorioRoom carroRepositorioRoom);
+
+    @Binds
+    public abstract MotocicletaRepositorio bindMotocicletaRepositorio(MotocicletaRepositorioRoom motocicletaRepositorioRoom);
+
+}
